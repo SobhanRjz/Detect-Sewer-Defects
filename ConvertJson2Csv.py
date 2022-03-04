32740f3743e41eafb29b7d96ae41cab8ce756086
SobhanRjz <Sobhan.Rajabzadeh@gmail.com>
2022-03-04 09:50:01 +0330

    Errors debuged and ConvertJson2Csv.py  can create csv file

--- ConvertJson2Csv.py
+++ ConvertJson2Csv.py
@@ -2,6 +2,7 @@
 from genericpath import exists
 import json
 import csv
+from operator import contains
 import os
 
 from numpy import matrix
@@ -16,7 +17,7 @@ def ReadJsonFile(Path):
     return data
 
 def WriteData2CSV(Path, data):
-    with open (Path, 'w+') as f :
+    with open (Path, 'w+', newline = '') as f :
         writer = csv.writer(f)
         writer.writerow(GetHeader())
         writer.writerows(data)
@@ -24,21 +25,22 @@ def WriteData2CSV(Path, data):
 def PrepareDataMatrix(data):
     MatrixList = []
     Catagories = (GetHeader())[1:]
-    ImageProp = [0] * (Catagories.count() + 1)
     for item in data:
-        Name = item["Name"]
+        ImageProp = [0] * (len(Catagories) + 1)
+        Name = data[item]["Name"]
         ImageProp[0] = Name
-        Catindex = [Catagories.index(i) if i in Catagories else None for i in item["Categories"]]
-        for i in Catindex: ImageProp[i] = 1
+        Catindex = [Catagories.index(i) if i in Catagories else None for i in data[item]["Categories"]]
+        if None in Catindex: continue
+        for i in Catindex: ImageProp[i + 1] = 1
         MatrixList.append(ImageProp) 
     return MatrixList
 
 def GetHeader():
-    return ['Image Name', 'Deposit', 'OpenJoint' , 'Washing', 'Spalling', 'Deformation', 'AttachedDeposit']
+    return ['Image Name', 'Deposit', 'OpenJoint' , 'Washing', 'Spalling', 'Deformation', 'AttachedDeposit', 'Nothing']
 
 def Main():
-    JsonPath = "H:\Video\PyProject\Labeled.json"
-    CsvFPath = "H:\Video\PyProject\CSVLabeled.csv"
+    JsonPath = "H:\Video\PyProject\OutPut\Labeled.json"
+    CsvFPath = "H:\Video\PyProject\OutPut\CSVLabeled.csv"
     data = ReadJsonFile(JsonPath)
     MatrixData = PrepareDataMatrix(data)
     WriteData2CSV(CsvFPath, MatrixData)
